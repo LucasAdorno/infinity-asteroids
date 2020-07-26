8fd8aacbb7c3413d33c30841f9f3dc0a290055ac
LucasAdorno <lucasadorno2702@gmail.com>
2020-07-26 01:05:01 -0300

    Start component info

---
+++ src/components/HeaderInfos/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container } from './styles';
+
+function HeaderInfos (){
+
+  return(
+    <Container>
+      <h1>1000</h1>
+      <h1>3</h1>
+    </Container>
+  );
+}
+
+export default HeaderInfos;
