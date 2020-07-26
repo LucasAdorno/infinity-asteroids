d5225c2b6decdedd3bc7f1021c052646fde3673a
LucasAdorno <lucasadorno2702@gmail.com>
2020-07-26 11:22:28 -0300

    Routes add

---
+++ src/Routes/routes.js
@@ -0,0 +1,21 @@
+
+import React from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+// import StartPage from '../Pages/StartPage';
+import GamePage from '../Pages/GamePage';
+// import EndPage from '../Pages/EndPage';
+
+const Routes = () => {
+
+  return(
+    <BrowserRouter>
+      <Switch>
+        {/* <Route exact path='/' component={StartPage}/> */}
+        <Route path='/' component={GamePage}/>
+        {/* <Route path='/gameover' component={EndPage}/> */}
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default Routes;
