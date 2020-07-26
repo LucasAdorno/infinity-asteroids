ed2123eafec12b3af81cfeb827ba998a0102b0f3
LucasAdorno <lucasadorno2702@gmail.com>
2020-07-26 20:49:06 -0300

    remove useEffect

--- src/components/HeaderInfos/index.js
+++ src/components/HeaderInfos/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Container } from './styles';
 import Heart from '../../assets/heart.png'
 
@@ -7,8 +7,6 @@ function HeaderInfos() {
 
   const [points, setPoints] = useState(0)
   const [life, setLife] = useState(3)
-
-  useEffect(() => {
     setInterval(() => {
       const lspoints = JSON.parse(localStorage.getItem('points'))
       const lslife = JSON.parse(localStorage.getItem('lifes'))
@@ -17,13 +15,12 @@ function HeaderInfos() {
         setLife(lslife)
       }
     }, 200)
-  }, [])
 
   return (
     <Container>
       <div className='header-info'>
         <div className='life-count'>
-          <img src={Heart} alt="life image"/>
+          <img src={Heart} alt="life mark"/>
           <h1> x {life}</h1>
         </div>
         <h1>{points}</h1>
