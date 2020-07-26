20aa1587f5f73d8fc84a7a123f536a37a1d7fd57
LucasAdorno <lucasadorno2702@gmail.com>
2020-07-26 11:21:54 -0300

    Page browser add

---
+++ src/Pages/GamePage/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import { Container } from './styles';
+import Game from '../../components/Game';
+import HeaderInfos from '../../components/HeaderInfos';
+
+
+const GamePage = () => {
+
+  return(
+    <Game/>
+    <HeaderInfos/>
+  );
+}
+
+export default GamePage;
