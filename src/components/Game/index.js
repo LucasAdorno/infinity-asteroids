58d91ab10f23ed1fe213cf7eb4e920b581c76848
LucasAdorno <lucasadorno2702@gmail.com>
2020-07-26 13:40:59 -0300

    Ajust of imports

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import P5Wrapper from 'react-p5-wrapper';
 import sketch from '../../sketches/sketch';
 import HeaderInfos from '../HeaderInfos';
