6a7da7636840862458f2316fe55fb39bb6a4c67f
LucasAdorno <lucasadorno2702@gmail.com>
2020-07-26 21:38:27 -0300

    Performance uptade

--- src/sketches/sketch.js
+++ src/sketches/sketch.js
@@ -11,78 +11,90 @@ export default function sketch(p) {
   let life = 3;
   let points = 0;
   let invenc = false;
+  let pos;
   localStorage.setItem('points', JSON.stringify(0));
   localStorage.setItem('lifes', JSON.stringify(3));
 
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight);
     p.noStroke();
+
     ship = new Ship(p);
+
     setInterval(() => {
-      if (life > 0){
-        if(asteroids.length < 30){
+      if (life > 0) {
+
+        if (asteroids.length < 30) {
           asteroids.push(new Asteroid(p));
         }
       }
+
       else {
         window.location.href = "/gameover";
-      }}, 800)
-      paralax[0] = new Paralax(p, 0.3);
-      paralax[1] = new Paralax(p, 0.6);
-      paralax[2] = new Paralax(p, 0.9);
+      }
+    }, 800)
+
+    paralax[0] = new Paralax(p, 0.3);
+    paralax[1] = new Paralax(p, 0.6);
+    paralax[2] = new Paralax(p, 0.9);
 
   }
 
   p.draw = () => {
-    p.background(0,0,0);
+    p.background(0, 0, 0);
 
-    for(let cenario of paralax){
-        cenario.update(p);
-        cenario.render(p);
-      };
+    paralax.forEach(scene => {
+      scene.update(p);
+      scene.render(p);
+    });
 
-    for (let i = 0; i < asteroids.length; i++) {
-      asteroids[i].render();
-      asteroids[i].update();
-      asteroids[i].offscreen();
+    asteroids.forEach(asteroid => {
+      asteroid.render();
+      asteroid.update();
+      asteroid.offscreen();
 
-      if (ship.hits(asteroids[i]) && invenc === false ) {
+      if (ship.hits(asteroid) && invenc === false) {
         life--;
         localStorage.setItem('lifes', JSON.stringify(life));
         invenc = true;
-        setTimeout(()=>{invenc = false}, 1000)
+        setTimeout(() => { invenc = false }, 800)
       }
-    }
+    })
 
     ship.render();
 
+    lasers.forEach(laser => {
+      laser.render();
+      laser.update();
 
-
-    for (let i = lasers.length - 1; i >= 0; i--) {
-      lasers[i].render();
-      lasers[i].update();
-      if (lasers[i].offscreen()) {
-        lasers.splice(i, 1)
+      if (laser.offscreen()) {
+        pos = lasers.indexOf(laser)
+        lasers.splice(pos, 1)
       }
+
       else {
-        for (let j = asteroids.length - 1; j >= 0; j--) {
-          if (lasers[i].hits(asteroids[j])) {
+        asteroids.forEach(asteroid => {
+
+          if (laser.hits(asteroid)) {
             points += 100;
             localStorage.setItem('points', JSON.stringify(points));
-            if (asteroids[j].r > 25) {
-              let newAsteroids = asteroids[j].breakup();
+
+            if (asteroid.r > 25) {
+              let newAsteroids = asteroid.breakup();
               asteroids = asteroids.concat(newAsteroids);
             }
-            asteroids.splice(j, 1);
-            lasers.splice(i, 1);
-            break;
+
+            pos = asteroids.indexOf(asteroid);
+            asteroids.splice(pos, 1);
+            pos = lasers.indexOf(laser)
+            lasers.splice(pos, 1);
           }
-        }
+        })
       }
-    }
+    })
     p.keyPressed = () => {
       if (p.key === 's' || p.key === 'S') {
-        if(lasers.length <= 10){
+        if (lasers.length <= 10) {
           lasers.push(new Laser(p))
         }
       }
