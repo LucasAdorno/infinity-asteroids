daa8dae0839d6d233d0e2e261d29409d451ebd0a
LucasAdorno <lucasadorno2702@gmail.com>
2020-07-28 00:55:51 -0300

    Refactor ship.js

--- src/sketches/ship.js
+++ src/sketches/ship.js
@@ -1,24 +1,22 @@
 export default class Ship {
 
   constructor(p) {
-    this.pos = p.createVector(p.width / 2, p.height / 2);
     this.r = 20;
   }
 
 
   render(p) {
     p.push();
-    p.translate(150, p.mouseY);
-    p.rotate(3.15 / 2);
+    p.translate(150, p.mouseY || p.windowHeight/2 );
     p.fill(0);
     p.stroke(255);
-    p.triangle(-this.r, this.r, this.r, this.r, 0, -this.r);
+    p.triangle(-this.r, -this.r, this.r, 0, -this.r, this.r);
     p.pop();;
   }
 
   hits(p, asteroid) {
-    let d = p.dist(150, p.mouseY, asteroid.pos.x, asteroid.pos.y);
-    if(d < this.r + asteroid.r){
+    let dist = p.dist(150, p.mouseY, asteroid.pos.x, asteroid.pos.y);
+    if(dist < this.r + asteroid.r){
       return true;
     }
     else{
