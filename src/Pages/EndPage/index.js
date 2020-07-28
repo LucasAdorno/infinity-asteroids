f74a71f1bc7af3588d946a8935d248e4dd881578
LucasAdorno <lucasadorno2702@gmail.com>
2020-07-28 03:40:44 -0300

    Record system add

--- src/Pages/EndPage/index.js
+++ src/Pages/EndPage/index.js
@@ -5,11 +5,18 @@ import Button from '../../components/Button';
 
 const EndPage = () => {
   let points = JSON.parse(localStorage.getItem('points'));
+  let record = JSON.parse(localStorage.getItem('record')) || 0;
+
+  if (points > record) {
+   localStorage.setItem('record', JSON.stringify(points));
+   record = points;
+  }
 
   return(
     <Container>
       <h1>Game Over</h1>
       <h1>Score: {points}</h1>
+      <h2>Record: {record}</h2>
       <Link to='/'><Button text='Home'/></Link>
     </Container>
     );
